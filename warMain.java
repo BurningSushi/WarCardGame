024265a9d75c79b98cc132425440500e9575c506
BurningSushi <babydoubles@gmail.com>
2015-09-23 16:18:16 -0600

    fixed deck creation and randomly pulling from deck to hand

--- warMain.java
+++ warMain.java
@@ -5,7 +5,7 @@ public class warMain{
    private static ArrayList<card> deck = new ArrayList<card>();
    private static ArrayList<card> p1 = new ArrayList<card>();
    private static ArrayList<card> p2 = new ArrayList<card>();
-   private static Random rand = new Random(deck.size()); //rand.nextInt()
+   private static Random rand = new Random(); //rand.nextInt()
    
    public static void main(String[] args){
       compileDeck();
@@ -15,21 +15,21 @@ public class warMain{
    
    private static void compileDeck(){
       int count = 5;
-      while(count < 10){
-         count ++;
-         deck.add(new card(count,"Hearts"));
-         deck.add(new card(count,"Spades"));
-         deck.add(new card(count,"Clubs"));
+      while(count < 11){
+         deck.add(new card(count,"Hearts "));
+         deck.add(new card(count,"Spades "));
+         deck.add(new card(count,"Clubs  "));
          deck.add(new card(count,"Dimonds"));
+         count ++;
       }
    }
    
    public static void dealHands(){
       int count = 0;
-      while(count < 1){
-         p1.add(deck.remove(rand.nextInt()));
-         p2.add(deck.remove(rand.nextInt()));
+      while(count < 9){
          count++;
+         p1.add(deck.remove(rand.nextInt(deck.size()-1)));
+         p2.add(deck.remove(rand.nextInt(deck.size()-1)));
       }
    }
    
@@ -38,6 +38,7 @@ public class warMain{
       int count = 0;
       while(count < 12){
          System.out.println(p1.get(count).toString() +"                 "+ p2.get(count).toString());
+         count++;
       }
    }
 }
